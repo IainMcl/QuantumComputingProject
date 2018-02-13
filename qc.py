4de5c1b3ad2aadf34ef71b42956f0d9e44e370b6
andee96 <s1416765@sms.ed.ac.uk>
2018-02-13 01:24:44 +0000

    update to qc.py - define class for hadamard gate and some basic examples

--- qc.py
+++ qc.py
@@ -25,14 +25,18 @@ Some notes from Andreas:
                 -Special class for fock states?
                 -Special qubit class that implements general
 
-            -Do we need something to convert integers to binary 
+            -Do we need something to convert integers to binary
+
+    -Probably need a class for grover's iterate defined as child class of more
+    general operator class?
 
 """
 
 import numpy as np
 from numpy.linalg import norm
-from abc import ABC, abstract method
+from scipy.sparse import coo_matrix, csc_matrix
 
+#this class might not be necessary
 class QuBit():
 
     def __init__(self, n_states, state ):
@@ -67,30 +71,31 @@ class QuBit():
 class QuantumRegister():
 
     def __init__(self, n_qubits):
-        """"
+        """
         Quantum register class. The quantum register is saved as a complex
         numpy array. Each element of the array is the amplitude of the
         corresponding state, eg. the first element is the first state, the second
         element is the second state.
-        """"
+        """
         self.n_states = 2 ** n_qubits
+        self.n_qubits = n_qubits
         self.qubits = np.zeros(self.n_states, dtype = complex)
-        qubits[0] = 1.0
+        self.qubits[0] = 1.0
 
 
     def set_qubit(self, a ,b):
         """
-
+        Set qubit of state a to value b
         """
+        pass
+
+
 
     def measure(self):
         """
-        Make a measurement. Square all the amplitudes and choose a random state
-
+        Make a measurement. Square all the amplitudes and choose a random state.
+        Outputs an integer representing the state measured (decimal system).
         """
-        #Square the amplitudes
-        #use np.random.choice and set probablilty to the probabiliy amplitudes calculated before
-
         #Calculate probabilites
         probabilities = np.zeros(n_states)
         for i in range(1, n_states):
@@ -103,7 +108,7 @@ class QuantumRegister():
 
 
 
-        def __mult__(self, other):
+    def __mult__(self, other):
         """
         Tensor prodcut between the two quantum registers. Outputs a new quantum
         register.
@@ -121,6 +126,12 @@ class QuantumRegister():
         return qmr_result
 
 
+    def normalise(self):
+        """
+        Normalise coefficients of qubits array
+        """
+        qubits_normalised = self.qubits/norm(self.qubits)
+        self.qubits = qubits_normalised
 
 
 
@@ -132,11 +143,17 @@ class Operator():
         -i,j: row nad column number
         -Mij: of the operator at that row and column
 
+    How to define the application of an operator to a quantum state? Method or override
+    multiplication.
+
+    Should there be an is_sparce property passed to the opeartor, with some default
+    value set?
     """
 
     def __init__(self, size):
-
-        pass
+        #Define number of columns and number of rows
+        self.size = size
+        self.sparce_matrix =
 
     def apply(self, quant_register):
     """
@@ -144,7 +161,86 @@ class Operator():
     register.
     """
 
+    def __mult__(self, other):
+        """
+        Overides multiplication operator so that the product between two operators
+        (assuming they have the same size) gives the correct result.
+        """
 
-class Hadamard(Operator):
+        if other.size != self.size:
+            print('Operators have to be the same size')
+            return None
+
+
+
+
+class Hadamard():
+    """
+    Class that defines hadamard gate.
+    """
 
     def __init__(self, size):
+        #super(Hadamard, self).__init__(size)
+
+        #Define "base" hadamard matrix for one qubit and correponding sparse matrix
+        self.base = 1/np.sqrt(2)*np.array( [ [1 , 1], [1 ,-1] ] )
+        self.base_sparse = coo_matrix(self.base)
+        self.matrix = self.__create_sparse_matrix(size)
+
+    def __create_sparse_matrix(self,size):
+        """
+        Create sparse matrix by calculating kronecker product of base matrix with
+        itself
+        """
+        result = self.base
+
+        if size == 1 :
+            return result
+        else:
+            for i in range(size-1):
+                result = np.kron(result,self.base)
+
+            return result
+
+
+    def apply(self, quant_register):
+        """
+        Apply hadamard gate to given quantum register
+
+        Vary number of inputs? If two inputs are submitted, then the first one
+        automatically becomes a control qubit?
+        """
+
+        #Initialize resulting quantum register
+        result = QuantumRegister( quant_register.n_qubits )
+
+        #Calculate result
+        result.qubits = np.dot(self.matrix, quant_register.qubits )
+
+        #Normalise result
+        result.normalise()
+
+        return result
+
+class cHadamard():
+    """
+    Class that defines controlled hadamard gate. Takes as inputs number of control
+    qubits and number of target qubits.
+    """
+
+    def __init(self, n_control, n_target):
+
+
+########testing stuff
+
+#Create 2-qubit hadamard gate
+H_2 = Hadamard(1)
+
+#Create a register with 2 qubits at the ground state
+ground_2 = QuantumRegister(1)
+print(ground_2.qubits)
+
+
+#matrix multiplication between
+test = H_2.apply(ground_2)
+print(test.qubits)
