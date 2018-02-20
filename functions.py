5b2a1d8c8ef7941e064dddb1048abd9f9656f889
andee96 <s1416765@sms.ed.ac.uk>
2018-02-20 16:30:03 +0000

    functions.py - extend deutsch algorithm to work for many qubits

--- functions.py
+++ functions.py
@@ -25,69 +25,73 @@ def deutsch(oracle):
     """
     Implements deutsch's algortithm to determine whether the oracle passed is
     balanced or not.
+
+    Extension of deutsch algorithm to n qubits
     """
+    #Extract number of qubits
+    n = oracle.n_qubits
+
     #Initialise both quantum registers
-    register1 = QuantumRegister()
+    register1 = QuantumRegister(n)
 
     not_gate = Not()
     h_gate = Hadamard()
     register2 = h_gate * not_gate  * QuantumRegister()
 
     #Pass register 1 through hadamard
-    register1 = h_gate * register1
+    h1 = Hadamard(n)
+    register1 = h1 * register1
     register1 = oracle * register1
 
     #Pass again through hadamard
-    final = h_gate * register1
+    final = h1 * register1
 
     #Perform measurement
     k = final.measure()
     return k
 
+ def grover_search(oracle):
+    """
+    implements grover's search algorithm at the given quantum register and
+    oracle, for just one element. Will later be extedned to multiple elements.
+    Inputs:
+    quant_register:
+    oracle: oracle function that "tags" a qubit in the quantum register
 
-# def grover_search(oracle):
-#     """
-#     implements grover's search algorithm at the given quantum register and
-#     oracle, for just one element. Will later be extedned to multiple elements.
-#     Inputs:
-#     quant_register:
-#     oracle: oracle function that "tags" a qubit in the quantum register
-#
-#     Outputs:
-#     measured_state: the state which the oracle tagged
-#     """
-#
-#     #Initialise quantum state and set it in superposition
-#     n = oracle.n_qubits
-#     register1 = QuantumRegister(n)
-#     h_gate = Hadamard(n)
-#     psi = h_gate * psi
-#     oracle_0 = Oracle()
-#     I = Operator(1, np.eye(2))
-#     resgiter2 = QuantumRegister()
-#     register.qubits = 1/np.sqrt(2)*np.array( [ 1, -1]) #will be done through unitary gate in the future: 1/sqrt(2) * (|0> - |1>)
-#     #Initialise grover's itearate
-#     c_fk = CUGate( oracle, n)
-#     c_f0 = CUGate( oracle_0, n)
-#
-#     term1 = c_fk.dot(h_gate*I)
-#     term2 = c_f0.dot(h_gate*I)
-#     grover_iterate = term1.dot(term2)
-#     #based on first paper for grover's iterate
-#
-#
-#     #for loop for grover search. At each iteration, apply grover's diffucion operator
-#
-#     #After for loop, perform measurement
-#
-#     k = psi.measure()
-#
-#     pass
+    Outputs:
+    measured_state: the state which the oracle tagged
+    """
+
+    #Initialise quantum state and set it in superposition
+    n = oracle.n_qubits
+    register1 = QuantumRegister(n)
+    h_gate = Hadamard(n)
+    psi = h_gate * psi
+    oracle_0 = Oracle()
+    I = Operator(1, np.eye(2))
+    resgiter2 = QuantumRegister()
+    register.qubits = 1/np.sqrt(2)*np.array( [ 1, -1]) #will be done through unitary gate in the future: 1/sqrt(2) * (|0> - |1>)
+    #Initialise grover's itearate
+    c_fk = CUGate( oracle, n)
+    c_f0 = CUGate( oracle_0, n)
+
+    term1 = c_fk.dot(h_gate*I)
+    term2 = c_f0.dot(h_gate*I)
+    grover_iterate = term1.dot(term2)
+    #based on first paper for grover's iterate
+
+
+    #for loop for grover search. At each iteration, apply grover's diffucion operator
+
+    #After for loop, perform measurement
+
+    k = psi.measure()
+    return k
 
 if __name__ == '__main__':
 
     #Create function
-    f = Operator(1, np.array( [ [1,0], [0,-1] ]))
+    f = Operator(10, np.array( [ [1,0], [0,-1] ]))
 
     k = deutsch(f)
 
