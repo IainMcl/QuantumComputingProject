d1f37e0fd457b3cb8fcd963a8b9f7d50694f7cda
Lewis <lewis@lappin.me.uk>
2018-02-20 14:32:15 +0000

    Added a phase shift multiplaction test method in ./Testying.py

--- Testing.py
+++ Testing.py
@@ -7,7 +7,7 @@ Created on Mon Feb 19 13:29:09 2018
 import unittest
 import numpy as np
 import math
-from qc import QuantumRegister, Hadamard,CHadamard, Oracle
+from qc_testing import QuantumRegister, Hadamard,CHadamard, Oracle, PhaseShift
 class QCTesting(unittest.TestCase):
     def test_mul_Hadamard(self):
         reg1 = QuantumRegister(1)
@@ -27,13 +27,25 @@ class QCTesting(unittest.TestCase):
         
         
     def test_oracle(self):
-        O = Oracle(5)
+        O = Oracle(10,x=5)
         reg = QuantumRegister(10)
         result = O*reg
         self.assertEqual(result.qubits[5],-1*reg.qubits[5])
         for i in range(reg.qubits.size):
             if i != 5:
                 self.assertEqual(result.qubits[i], reg.qubits[i])
+                
+                
+    def test_phase_shift(self):
+        P = PhaseShift(math.pi, n_qubits = 1)
+        H = Hadamard(1)
+        reg = H*QuantumRegister(1)
+        regQuBits = reg.qubits
+        result = P*reg
+        
+        gate = np.array([[1,0],[0,-1]])
+        expected = np.dot(gate,regQuBits)  
+        np.testing.assert_almost_equal(expected.tolist(),result.qubits.tolist())
         
         
 unittest.main()
