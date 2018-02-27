e28e27ea1c9d2dcfb0c87c799d0ad221bbc2aad3
Lewis <lewis@lappin.me.uk>
2018-02-27 11:07:37 +0000

    created an oracle object that hopefully solves the travelling saleman problem

--- lewisMADgrover.py
+++ lewisMADgrover.py
@@ -8,6 +8,10 @@ Created on Mon Feb 26 17:37:45 2018
 from qc import *
 from functions import *
 import numpy as np
+from scipy.spatial import distance
+from sympy.utilities.iterables import multiset_permutations
+from itertools import permutations
+
 
 def grover_search(oracle):
     n = oracle.n_qubits
@@ -36,17 +40,39 @@ def grover_search(oracle):
     k = reg.measure()
     return k
 
-class Oracle():
-    def __init__(self, x):
-        self.x = x
-    def __getitem__(self, key):
-        if key == x:
+class TSPOracle():
+    def __init__(self, locations,threshold):
+        self.locations = locations
+        self.locationPerms = tuple(permutations(self.locations))
+        self.threshold = threshold
+        
+    def calcDistance(self, locationArray):
+        dist = 0
+        for i in range(len(locationArray)-1):
+            dist += distance.euclidean(locationArray[i],locationArray[i+1])
+            
+        return dist
+    
+    def __getitem__(self,key):
+        dist = self.calcDistance(self.locationPerms[key])
+        print(dist)
+        if dist < self.threshold:
             return 1
         else:
             return 0
 
-
 if __name__=='__main__':
-    oracle = Oracle(x=3,n_qubits = 10)
-    k = grover_search(oracle)
-    print(k)
+    #oracle = Oracle(x=3,n_qubits = 10)
+    #k = grover_search(oracle)
+    #print(k)
+    locations = np.array([[0,1],[3,4],[4,3]])
+    
+    
+    o = TSPOracle(locations, 5)
+    for i in range(6):
+        o[i]
+    
+    
+    
+    
+    
