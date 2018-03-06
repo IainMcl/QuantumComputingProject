37a6b2deab90f4d582da921424757490ab143a61
andee96 <s1416765@sms.ed.ac.uk>
2018-03-06 15:25:36 +0000

    working version of grover for 5 qubits with predifined oracle

--- grover5_andy.py
+++ grover5_andy.py
@@ -1,12 +1,11 @@
 """
-Testing grover. Parts are lazily implemented (such as controlled - n gates.
-Based on Quantum Computer Science book, Chapter 4
+Testing random stuff
 """
 
 # Test implementation of Grover's algorithm for 3 qubits with a 3 qubit oracle.
 from qc_simulator.qc import *
 import numpy as np
-
+import math
 
 
 def grover5():
@@ -25,35 +24,40 @@ def grover5():
     # Define the inversion about average operator
     # W = H**n % X**n % (cn^-1 Z) * X%%n * H%%
 
-    #define z and control z_gates
+    # Define z and control z_gates
     z = PhaseShift(np.pi)
-    control_z = CUGate(z, 5)
+    control_z = CUGate(z, 4)
     h_n_gate = Hadamard(6)
     not_n_gate = Not(6)
 
-    W = h_n_gate * not_n_gate * control_z * not_n_gate * h_n_gate
-    test = W * QuantumRegister(6)
+    W = h_n_gate * not_n_gate * (control_z % I) * not_n_gate * h_n_gate
+    test = W * ( Hadamard(6) * QuantumRegister(6) )
     test.remove_aux(1/np.sqrt(2))
 
     G = W * oracle_gate
 
     # Define the input and ancillary quantum registers
-    input_register = Hadamard(5) *  QuantumRegister(5)
+    input_register = Hadamard(5) * QuantumRegister(5)
     aux = h_gate * not_gate * QuantumRegister()
     register = input_register
 
     # Loop and apply grover operator iteratively
-    for i in range(10):
+    n = math.ceil( math.sqrt(5) )
+    for i in range(n):
         # Add auxilary qubit to register
         register = register * aux
 
         # Apply grover iteration
-        register = G * register
+        register = oracle_gate * register
+        register.remove_aux(1/np.sqrt(2))
+        register = register* aux
+        register = W * register
+        #register.remove_aux(1/np.sqrt(2))
 
-        #Extrac input register and reset auxillary qubit (hacky way)
+        # Extract input register and reset auxillary qubit (hacky way)
         register.remove_aux(1/np.sqrt(2))
 
-        aux =  h_gate * not_gate * QuantumRegister()
+        aux = h_gate * not_gate * QuantumRegister()
 
 
     n = register.measure()
