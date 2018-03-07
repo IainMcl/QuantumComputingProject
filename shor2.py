114546bce40011ae2abda8a965aa355f9d0ad5b7
john <36479901+brotwasme@users.noreply.github.com>
2018-03-07 17:00:54 +0000

    shor, QTF started will edit

---
+++ shor2.py
@@ -0,0 +1,30 @@
+import numpy as np
+from qc_simulator.qc import *
+
+class shors:
+    """
+    """
+
+    def QFT(self, QR, n):
+        """
+        maybe to be edited, if statment changed maybe
+        """
+        H = Hardamard()
+        I = IdentityGate()
+        P = PhaseShift()
+        for j in range(n):
+            if j = 0:
+                M1 = IdentityGate(n-1)%H
+            if j = n-1:
+                M1 = H%IdentityGate(n-1)
+            else:
+                M1 = IdentityGate(j)%H%IdentityGate(n-1-j)
+                M2 = I
+            for i in rang(n-j):
+                M2 = M2%I
+            for i in range(j):
+                M2 = P%M2
+            for j=0: #check about identity
+                M = M2*M1
+            else:
+                M = M2*M1*M
