78e4da7b38c90ce3a71f6432a02771f52bc623a0
Lewis <lewis@lappin.me.uk>
2018-03-07 15:44:42 +0000

    add functional oracle

---
+++ FunctionalOracle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  7 14:49:33 2018
+
+@author: Lewis
+"""
+
+class functional_oracle(Operator):
+    def __init__(self, function, n_qubits):
+        
