263c4b89d71ea15c4b0cd072f139e5fcae6c1885
john <36479901+brotwasme@users.noreply.github.com>
2018-03-08 14:12:23 +0000

    edited carrygate so it works

--- shor2.py
+++ shor2.py
@@ -40,7 +40,7 @@ class shors:
             M = M2*M1*M
         return M
 
-    def Sum(self, n=3):
+    def SumGate(self, n=3):
         """
         unlikely that it can be applied to n =\=3
         but will keep just in case for now, only used as n = 3
@@ -50,14 +50,18 @@ class shors:
         M = CUGate(Not(),1,1,1)*M
         return M
     
-    def Carry(self, n = 4):
+    def CarryGate(self, n = 4):
         """
         like sum, likely only usable with 4
         """
         I = IdentityGate()
-        M = I%CUGate(Not(),2,1)
+        M = I%build_c_c_not()
         M = (I%CUGate(Not())%I)*M
-        M = M1*M #CQubit I CQubit TQubit
+        M = build_c_c_not(1,0)*M #CQubit I CQubit TQubit
+        return M
+
+    def AdderGate(self):
+        print("addder")
 
 
     def Shors(self, QR1, QR2):
